67a6144db440ecfeb05de29c3ba617ab4ea8ae28
naomi.stern <naomi.stern@ft.com>
2017-08-10 09:53:39 +0100

    array of same suit calculations

--- lib/pokerwars/hand.ex
+++ lib/pokerwars/hand.ex
@@ -1,102 +1,23 @@
 defmodule Pokerwars.Hand do
-  def score(cards) do
-    cards = Enum.sort(cards)
-    calculate_score(cards)
-  end
 
-  defp calculate_score(cards) do
-    cond do
-      straight_flush?(cards) -> :straight_flush
-      four_of_a_kind?(cards) -> :four_of_a_kind
-      flush?(cards) -> :flush
-      full_house?(cards) -> :full_house
-      straight?(cards) -> :straight
-      three_of_a_kind?(cards) -> :three_of_a_kind
-      two_pair?(cards) -> :two_pair
-      pair?(cards) -> :pair
-      true -> :high_card
+    def score(cardArray) do
+        suits = Enum.map(cards, fn x -> x[:suit] end)
+        ranks = Enum.map(cards, fn x -> x[:rank] end)
+
+        cond do
+          first_suit = suits[0]
+          array_of_same_suit(suits, first_suit)
+        end
     end
-  end
 
-  defp pair?(cards) do
-    ranks = extract_ranks(cards)
-    case ranks do
-      [a, a, _, _, _] -> true
-      [_, a, a, _, _] -> true
-      [_, _, a, a, _] -> true
-      [_, _, _, a, a] -> true
-      _ -> false
+
+    def array_of_same_suit([head | tail], first_suit) do
+        cond do
+          head != first_suit -> false
+          empty?(tail) -> true
+          _ -> array_of_same_suit(tail, first_suit)
+        end
     end
-  end
-
-  defp two_pair?(cards) do
-    ranks = extract_ranks(cards)
-    case ranks do
-      [a, a, b, b, _] -> true
-      [a, a, _, b, b] -> true
-      [_, a, a, b, b] -> true
-      _ -> false
-    end
-  end
-
-  defp three_of_a_kind?(cards) do
-    ranks = extract_ranks(cards)
-    case ranks do
-      [a, a, a, _, _] -> true
-      [_, a, a, a, _] -> true
-      [_, _, a, a, a] -> true
-      _ -> false
-    end
-  end
-
-  defp four_of_a_kind?(cards) do
-    ranks = extract_ranks(cards)
-    case ranks do
-      [a, a, a, a, _] -> true
-      [_, a, a, a, a] -> true
-      _ -> false
-    end
-  end
-
-  defp flush?(cards) do
-    suits = extract_suits(cards)
-    case suits do
-      [a,a,a,a,a] -> true
-      _ -> false
-    end
-  end
-
-  defp straight?(cards) do
-    ranks = extract_ranks(cards)
-
-    ranks == [2,3,4,5,14] or
-    consecutive?(ranks)
-  end
-
-  defp straight_flush?(cards) do
-    straight?(cards) and
-    flush?(cards)
-  end
-
-  defp full_house?(cards) do
-    ranks = extract_ranks(cards)
-    case ranks do
-      [a, a, b, b, b] -> true
-      [b, b, b, a, a] -> true
-      _ -> false
-    end
-  end
-
-  defp extract_ranks(cards) do
-    Enum.map(cards, fn x -> x.rank end)
-  end
-
-  defp extract_suits(cards) do
-    Enum.map(cards, fn x -> x.suit end)
-  end
-
-  defp consecutive?([_a]), do: true
-  defp consecutive?([a | [b | t]]) do
-   a + 1 == b and consecutive?([b | t])
-  end
 end
+
+
