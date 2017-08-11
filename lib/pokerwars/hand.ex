6c87b44a38b45cf17d6fcf4bb1a3cfd2a0555c6f
naomi.stern <naomi.stern@ft.com>
2017-08-11 10:07:49 +0100

    array of same suit calculations

--- lib/pokerwars/hand.ex
+++ lib/pokerwars/hand.ex
@@ -1,12 +1,13 @@
 defmodule Pokerwars.Hand do
 
     def score(cardArray) do
-        suits = Enum.map(cards, fn x -> x[:suit] end)
-        ranks = Enum.map(cards, fn x -> x[:rank] end)
+        suits = Enum.map(cardArray, fn x -> x[:suit] end)
+        ranks = Enum.map(cardArray, fn x -> x[:rank] end)
+         first_suit = suits[0]
 
         cond do
-          first_suit = suits[0]
-          array_of_same_suit(suits, first_suit)
+          array_of_same_suit(suits, first_suit) and ascending_rank(ranks) -> :straight_flush
+          true -> "no idea"
         end
     end
 
@@ -14,8 +15,17 @@ defmodule Pokerwars.Hand do
     def array_of_same_suit([head | tail], first_suit) do
         cond do
           head != first_suit -> false
-          empty?(tail) -> true
-          _ -> array_of_same_suit(tail, first_suit)
+          is_list?(tail) != true -> true
+          true -> array_of_same_suit(tail, first_suit)
+
+        end
+    end
+
+    def ascending_rank([first | [second | tail]]) do
+        cond do
+          (first + 1) != second -> false
+          isList?(tail) -> true
+          true -> ascending_rank([second | tail])
         end
     end
 end
